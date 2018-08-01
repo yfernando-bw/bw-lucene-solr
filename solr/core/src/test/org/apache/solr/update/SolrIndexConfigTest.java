b6400fe4d3c2c40146637ace99a8aa7c9cb5128b
Steve Hunt <steve@brandwatch.com>
2018-08-01 17:52:56 +0100

    Fixing default for doAutoIOThrottle / isAutoIOThrottle in recent unit test, to match BW specific hard-coding

--- solr/core/src/test/org/apache/solr/update/SolrIndexConfigTest.java
+++ solr/core/src/test/org/apache/solr/update/SolrIndexConfigTest.java
@@ -94,7 +94,7 @@ public class SolrIndexConfigTest extends SolrTestCaseJ4 {
     ConcurrentMergeScheduler ms = (ConcurrentMergeScheduler)  iwc.getMergeScheduler();
     assertEquals("ms.maxMergeCount", 987, ms.getMaxMergeCount());
     assertEquals("ms.maxThreadCount", 42, ms.getMaxThreadCount());
-    assertEquals("ms.isAutoIOThrottle", true, ms.getAutoIOThrottle());
+    assertEquals("ms.isAutoIOThrottle", false, ms.getAutoIOThrottle());
 
   }
 
